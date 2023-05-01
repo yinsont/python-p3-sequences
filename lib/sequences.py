1c19d29fab27388990d0f2d63fa10bc3ce0cc5b4
Yinson Tso <yinson.tso@gmail.com>
2023-05-01 09:52:43 -0400

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+  
+
+  if length == 0:
+    a = []
+  elif length == 1:
+    a = [0]
+  else:
+    a = [0,1]
+    for n in range(length-2):
+        number = a[-1]
+        number2 = a[-2]
+        a.append(number + number2)
+    
+  print(a)
